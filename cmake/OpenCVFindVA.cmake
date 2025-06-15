27aa58bb39baa92e81e79fb4ba98eaea800bd201
Didier Garcin <didu31@hotmail.fr>
2025-06-15 22:14:35 +0200

    Modifications for successful installation on FreeBSD

--- cmake/OpenCVFindVA.cmake
+++ cmake/OpenCVFindVA.cmake
@@ -2,6 +2,8 @@
 #   HAVE_VA - libva is available
 #   HAVE_VA_INTEL - OpenCL/libva Intel interoperability extension is available
 
+include(FindPkgConfig)
+
 find_path(
     VA_INCLUDE_DIR
     NAMES va/va.h
@@ -9,7 +11,8 @@ find_path(
     PATH_SUFFIXES include
     DOC "Path to libva headers"
 )
-
+pkg_search_module (VA libva)
+pkg_search_module (VA_DRM libva-drm)
 if(VA_INCLUDE_DIR)
     set(HAVE_VA TRUE)
     if(NOT DEFINED VA_LIBRARIES AND NOT OPENCV_LIBVA_LINK)
